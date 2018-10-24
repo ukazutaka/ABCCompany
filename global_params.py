7f6df48dbfd6a11c55d1e3808e4debfb3dbd5145
ukazutaka <41758192+ukazutaka@users.noreply.github.com>
2018-10-24 15:22:37 -0700

    Create global_params.py

---
+++ global_params.py
@@ -0,0 +1,29 @@
+# enable exceptions instead of ignoring them
+IGNORE_EXCEPTIONS = 0
+
+# enable reporting of the result
+REPORT_MODE = 0
+
+#print everything in the console
+PRINT_MODE = 0
+
+# enable data flow analysis (incomplete yet)
+DATA_FLOW = 0
+
+# enable log file to print all exception
+DEBUG_MODE = 1
+
+# check false positive in concurrency
+CHECK_CONCURRENCY_FP = 0
+
+# Timeout for z3
+TIMEOUT = 1000
+
+# Set this flag to 1 if we want to do unit test
+UNIT_TEST = 2
+
+# timeout to run symbolic execution (in secs)
+GLOBAL_TIMEOUT = 300
+
+# print path conditions
+PRINT_PATHS = 0
